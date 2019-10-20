35375b9d6b5834965bb9c3fd6dad7e5528450bb9
cptwalmart <cptwalmart@gmail.com>
2019-10-20 13:57:48 -0400

    Add files via upload

--- P240/240.c
+++ P240/240.c
@@ -1,138 +1,52 @@
-// 420 Project 1
-// Problem #240 60% difficult
-// Hardcoded for mpiexec -n 72 ./Proj240
-// How to compile
-// make
-
 #include <stdio.h>
-#include <mpi.h>
-#include <math.h>
-#include <stdbool.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
 
-int cmpfunc (const void * a, const void * b) {
-   return ( *(int*)a - *(int*)b );
+#define DICE 20
+#define PICK 10
+#define SIDES 12
+#define TARGET 70
+
+long factorial(long i) {
+  long j, r = 1;
+  for(j = 1; j <= i; j++) {
+    r *= j;
+  }
+  return r;
 }
 
-int Loop(int x, int y, int* List){
-    int Count = 0;
+long sum = 0;
 
-    for(int a=x;a<=x;a++){
-        for(int b=y;b<=(y+1);b++){
-            for(int c=1;c<=12;c++){
-                for(int d=1;d<=12;d++){
-                    for(int e=1;e<=12;e++){
-                        for(int f=1;f<=12;f++){
-                            for(int g=1;g<=12;g++){
-                                for(int h=1;h<=12;h++){
-                                    for(int i=1;i<=12;i++){
-                                        for(int j=1;j<=12;j++){
-                                            for(int k=1;k<=12;k++){
-                                                for(int l=1;l<=12;l++){
-                                                    for(int m=1;m<=12;m++){
-                                                        for(int n=1;n<=12;n++){
-                                                            for(int o=1;o<=12;o++){
-                                                                for(int p=1;p<=12;p++){
-                                                                    for(int q=1;q<=12;q++){
-                                                                        for(int r=1;r<=12;r++){
-                                                                            for(int s=1;s<=12;s++){
-                                                                                for(int t=1;t<=12;t++){
-                                                                                    List[0]=a;
-                                                                                    List[1]=b;
-                                                                                    List[2]=c;
-                                                                                    List[3]=d;
-                                                                                    List[4]=e;
-                                                                                    List[5]=f;
-                                                                                    List[6]=g;
-                                                                                    List[7]=h;
-                                                                                    List[8]=i;
-                                                                                    List[9]=j;
-                                                                                    List[10]=k;
-                                                                                    List[11]=l;
-                                                                                    List[12]=m;
-                                                                                    List[13]=n;
-                                                                                    List[14]=o;
-                                                                                    List[15]=p;
-                                                                                    List[16]=q;
-                                                                                    List[17]=r;
-                                                                                    List[18]=s;
-                                                                                    List[19]=t;
-                                                                                    qsort(List, 20, sizeof(int), cmpfunc);
-                                                                                    if((List[19]+List[18]+List[17]+List[16]+List[15]+List[14]+List[13]+List[12]+List[11]+List[10])==70){
-                                                                                        Count++;
-                                                                                        //printf("%d %d %d %d %d %d %d %d %d %d %d\n",Count,List[19],List[18],List[17],List[16],List[15],List[14],List[13],List[12],List[11],List[10]);
-                                                                                    }
-                                                                                }
-                                                                            }
-                                                                        }
-                                                                    }
-                                                                }
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }   
-                                    }   
-                                }   
-                            }   
-                        }        
-                    }                
-                }
-            }
-        }
+void iterate_dice_reentrant(int index, int *dice) {
+  if(index == DICE) {
+    int i;
+    int counts[SIDES] = {0};
+    for(i = 0; i < DICE; i++) {
+      counts[dice[i]-1]++;
     }
-    return Count;
+    long n_perms = factorial(DICE);
+    for(i = 0; i < SIDES; i++) {
+      if(counts[i] == 0) continue;
+      n_perms /= factorial(counts[i]);
+    }
+    sum += (long) n_perms;
+    return;
+  }
+  if(index == PICK) {
+    int sum = 0, i;
+    for(i = 0; i < index; i++) sum += dice[i];
+    if(sum != TARGET) return;
+  }
+  int last = SIDES;
+  if(index > 0) last = dice[index-1];
+  int i;
+  for(i = 1; i <= last; i++) {
+    dice[index] = i;
+    iterate_dice_reentrant(index + 1, dice);
+  }
 }
 
-int main(int argc, char* argv[]){
-    MPI_Init(NULL, NULL);
-
-    MPI_Comm world = MPI_COMM_WORLD;
-
-    int cores;
-    MPI_Comm_size(world, &cores);
-    int rank;
-    MPI_Comm_rank(world, &rank);
-
-    unsigned long long Count = 0;
-    unsigned long long Add = 0;
-
-    int List[20];
-    int x = 1;
-    int y = 1;
-
-    if(cores == 72){
-        for(int Start = 1; Start<72; Start++){
-            if(rank == Start){
-                Count=Count+Loop(x,y,List);
-                MPI_Send(&Count, 1, MPI_UNSIGNED_LONG_LONG, 0, Start, world);
-                y = y + 2;
-            }
-            if(Start%12 == 5){
-                x = x + 1;
-            }
-            if(Start%6 == 0){
-                y = 1;
-            }
-        }
-
-
-        if(rank == 0){
-            for(int Start = 0; Start<72; Start++){
-                MPI_Recv(&Add, 1, MPI_UNSIGNED_LONG_LONG, Start, Start, world, MPI_STATUS_IGNORE);
-                Count = Count + Add;
-            }
-            printf("\n\nANSWER ANSWER ANSWER %d ANSWER ANSWER ANSWER\n\n",Count);
-        }
-
-        printf("Rank:%d Count:%d\n",rank,Count);
-    }
-
-    MPI_Finalize();
-
-    return 0;
+int main(void) {
+  int *current_dice = calloc(DICE, sizeof(int));
+  iterate_dice_reentrant(0, current_dice);
+  printf("%ld\n", sum);
 }
